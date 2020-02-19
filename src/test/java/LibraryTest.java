226ebcd30183ccaabf46ec68dc69023a8aef6d15
Piyush Yadav <31996687+piyush5989@users.noreply.github.com>
2020-02-19 18:04:59 +0530

    testing heuristic

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -5,8 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test
-    public void testSomeLibraryMethod() {
+    @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
